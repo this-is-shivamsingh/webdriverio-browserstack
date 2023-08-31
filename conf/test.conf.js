e25b113ffe22ede83e465d4851cb9030babc1cae
this-is-shivamsingh <shivam.si@browserstack.com>
2023-08-31 15:42:58 +0530

    added test.conf.js

--- conf/test.conf.js
+++ conf/test.conf.js
@@ -1,49 +1,44 @@
-const { config: baseConfig } = require('./base.conf.js');
-
 const parallelConfig = {
-  maxInstances: 10,
-  commonCapabilities: {
-    'bstack:options': {
-      buildName: 'browserstack build',
-      source: 'webdriverio:sample-master:v1.2'
-    }
-  },
-  services: [
-    [
-      'browserstack',
-      { buildIdentifier: '#${BUILD_NUMBER}' },
-    ],
-  ],
+  user: 'shivamsingh_5WHsDt',
+  key: 'QW5MbjdhQCRQnndzo1Mx',
+  hostname: 'hub.browserstack.com',
   capabilities: [
     {
-      browserName: 'chrome',
-      browserVersion: 'latest',
+      browserName: 'Chrome',
+      'bstack:options': {
+        os: 'Windows',
+        osVersion: '11',
+        browserVersion: '103.0'
+      }
+    },
+    {
+      browserName: 'Firefox',
       'bstack:options': {
         os: 'Windows',
         osVersion: '10',
-      },
+        browserVersion: '102.0'
+      }
     },
     {
-      browserName: 'safari',
-      browserVersion: 'latest',
+      browserName: 'Safari',
       'bstack:options': {
         os: 'OS X',
         osVersion: 'Big Sur',
-      },
-    },
-    {
-      browserName: 'chrome',
-      'bstack:options': {
-        deviceName: 'Samsung Galaxy S20',
-      },
-    },
+        browserVersion: '14.1'
+      }
+    }
   ],
+  commonCapabilities: {
+    'bstack:options': {
+      buildName: 'browserstack-build-1'
+    }
+  },
+  maxInstances: 10
 };
-
+const { config: baseConfig } = require('./base.conf.js');
 exports.config = { ...baseConfig, ...parallelConfig };
-
 // Code to support common capabilities
-exports.config.capabilities.forEach(function (caps) {
+exports.config.capabilities.forEach(function(caps) {
   for (var i in exports.config.commonCapabilities)
-    caps[i] = { ...caps[i], ...exports.config.commonCapabilities[i]};
+    caps[i] = { ...caps[i], ...exports.config.commonCapabilities[i] };
 });
